d499c2d4f75d67e5b5902e2cbfeb5547acf67593
Koehler <bernardoskoehler@gmail.com>
2026-03-27 12:02:27 -0300

    Delete scr/ContaPadrao.java

--- scr/ContaPadrao.java
+++
@@ -1,7 +0,0 @@
-public class ContaPadrao extends ContaBase{
-
-    ContaPadrao(String titular, double saldo, String senha) {
-        super(titular, saldo, senha);
-        this.valorMaximo = 1000;
-    }
-}
