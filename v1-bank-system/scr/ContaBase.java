28393bf944fbb7c75b54a2446d190ce3dc256ad1
Koehler <bernardoskoehler@gmail.com>
2026-04-23 16:05:20 -0300

    Update ContaBase.java

--- v1-bank-system/scr/ContaBase.java
+++ v1-bank-system/scr/ContaBase.java
@@ -1,201 +1,172 @@
-import java.util.Scanner;
 import java.util.ArrayList;
 
 abstract class ContaBase {
-    Scanner scanner = new Scanner(System.in);
-    private String titular;
-    private double saldo;
-    private String senha;
-
-    private int tentativasValidas = 3;
-    private boolean contaAtiva = false;
     private ArrayList<String> historico = new ArrayList<>();
+    private String nome;
 
-    protected double valorMaximo;
+    private double saldo;
+    protected double limite;
 
-    ContaBase(String titular, double saldo, String senha) {
-        setTitular(titular);
-        setSaldo(saldo);
+    private String senhaAtual;
+    private int tentativas = 5;
+
+    private boolean contaAtiva = false;
+
+    ContaBase(String nome, String senha) {
+        setNome(nome);
         setSenha(senha);
     }
 
-    /// /// /// /// /// Get/Set /// /// /// /// ///
-    private void setTitular(String titular) {
-        if (titular != null && !titular.trim().isEmpty()) {
-            this.titular = titular;
-            return;
-        }
-        System.out.println("Nome de titular invalido, nome padrao *Null* adotado.");
-        this.titular = "Null";
-    }
-    protected String getTitular() {
-        return titular;
-    }
-    private void setSaldo(double saldo) {
-        if (!valorValido(saldo)) {
-            System.out.println("Saldo inserido invalido, saldo padrao *100.00* adotado.");
-            return;
-        }
-        this.saldo = saldo;
-    }
-    protected double getSaldo() {
-        return saldo;
-    }
-    private void setSenha(String senha) {
-        if (titular != null && !senha.trim().isEmpty() && senha.length() >= 8) {
-            this.senha = senha;
-            return;
-        }
-        System.out.println("Senha inserida invalida, senha padrao *12345678* adotada.");
-        this.senha = "12345678";
-    }
-    private String getSenha() {
-        return senha;
-    }
-    protected int getTentativasValidas() {
-        return tentativasValidas;
-    }
-
-    /// /// /// /// /// Get/Set /// /// /// /// ///
-    /// /// /// /// /// Validações /// /// /// ///
-    protected boolean valorValido(double valor) {
-        if (valor > 0) {
+    public boolean valorValido(double valor) {
+        if(valor > 0) {
             return true;
         }
         System.out.println("Valor inserido invalido!");
         return false;
     }
 
-    protected boolean contaEstaAtiva() {
-        if(contaAtiva) {
+    public boolean temTentativas() {
+        if(tentativas > 0) {
             return true;
         }
-        System.out.println("Acesse a conta primeiro");
+        System.out.println("Nao tem mais tentativas!");
+        return false;
+    }
+    public boolean verificarSenha(String senha) {
+        if(senha != null && senha.equals(senhaAtual)) {
+            return true;
+        }
+        tentativas --;
+        System.out.println("Tentativas restantes: " +tentativas);
         return false;
     }
 
-    protected boolean temTentativas() {
-        if(getTentativasValidas() <= 0) {
-            System.out.println("Nao tem mais tentativas");
+    protected void ativarConta(String senha) {
+        if(contaEstaAtiva()) {
+            System.out.println("A conta ja esta ativa!");
+            return;
         }
-        return getTentativasValidas() > 0;
-    }
-    private boolean tentativaSenha() {
         if(!temTentativas()) {
-            System.out.println("Você nao tem mais tentativas.");
-            return false;
+            return;
         }
-        System.out.println("Digite a senha atual:");
-        String tentativaSenha = scanner.nextLine();
-        if(!tentativaSenha.equals(getSenha())){
-            tentativasValidas --;
-            System.out.println("Senha inseria incorreta, tentativas restantes: " +getTentativasValidas());
-            return  false;
+        if(!verificarSenha(senha)) {
+            return;
         }
-        tentativasValidas = 3;
-        return true;
+        contaAtiva = true;
+        System.out.println("Conta: " +getNome() +" Ativa.");
+        tentativas = 5;
     }
-    private boolean podeSacar(double valor) {
-        if(!valorValido(valor)) {
-            return false;
-        }
-        if(getSaldo() - valor < 0) {
-            System.out.println("Nao tem saldo o suficiente");
-            return false;
-        }
-        if(valor > valorMaximo) {
-            System.out.println("Valor é maior que limite de saque");
+    public boolean contaEstaAtiva() {
+        if(contaAtiva == false) {
+            System.out.println(nome +" ative a conta");
             return false;
         }
         return true;
-    }
-    /// /// /// /// /// Validações /// /// /// ///
-    protected void ativarConta() {
-        if (contaEstaAtiva()) {
-            return;
-        }
-        if(tentativaSenha()) {
-            contaAtiva = true;
-            System.out.println("Você acessou a conta do " + getTitular());
-        }
-    }
-    protected void mudarSenha() {
-        if(!contaEstaAtiva()) {
-            return;
-        }
-        if(tentativaSenha()) {
-            System.out.println("Digite uma nova senha:");
-            String novaSenha = scanner.nextLine();
-            setSenha(novaSenha);
-            return;
-        }
-    }
-    protected void mudarTitular() {
-        if(!contaEstaAtiva()) {
-            return;
-        }
-        if(tentativaSenha()) {
-            System.out.println("Digite um novo nome:");
-            String novoTitular = scanner.nextLine();
-            setTitular(novoTitular);
-            return;
-        }
-    }
-    private void adicionarSaldo(double valor) {
-        saldo += valor;
-    }
-    private void removerSaldo(double valor) {
-        saldo -= valor;
     }
 
-    protected void depositar(double valor){
-        if(!contaEstaAtiva()) {
+    private void setNome(String nome) {
+        if(nome != null && !nome.trim().isEmpty()) {
+            this.nome = nome;
             return;
         }
+        System.out.println("Nome invalido, nome padrao *NULL* adotado!");
+        this.nome = "NULL";
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void mudarNome(String nome) {
+        if(contaEstaAtiva()) {
+            setNome(nome);
+        }
+    }
+    private boolean senhaValida(String senha) {
+        return senha != null && senha.length() >= 8;
+    }
+    private void setSenha(String senha) {
+        if(!senhaValida(senha)) {
+            System.out.println("Senha invalida, senha padrao *12345678* adotada!");
+            this.senhaAtual = "12345678";
+            return;
+        }
+        this.senhaAtual = senha;
+    }
+    public void mudarSenha(String senhaAtual, String novaSenha) {
+        if(!verificarSenha(senhaAtual)) {
+            return;
+        }
+        setSenha(novaSenha);
+    }
+
+    public boolean podeRemoverSaldo(double valor) {
         if(!valorValido(valor)) {
-            System.out.println(getTitular() +" Nao foi pocivel depositar quantia.");
-            return;
+            return false;
         }
-        System.out.println(getTitular() +" depositou R$" +valor);
-        adicionarSaldo(valor);
-        historico.add( "+" + String.valueOf(valor));
+        if(valor > limite) {
+            System.out.println("R$ " +valor +" esta acima do limite da conta");
+            return false;
+        }
+        if(saldo < valor) {
+            System.out.println("Nao tem saldo o suficiente!");
+            return false;
+        }
+        return true;
     }
-    protected void sacar(double valor) {
+    private boolean removerSaldo(double valor) {
+        if(!podeRemoverSaldo(valor)) {
+            return false;
+        }
+        System.out.println("R$ " +valor +" removido de " +getNome());
+        saldo -= valor;
+        return true;
+    }
+    private boolean adicionarSaldo(double valor) {
+        if(!valorValido(valor)) {
+            return false;
+        }
+        System.out.println("R$ " +valor +" adicionado a " +getNome());
+        saldo += valor;
+        return true;
+    }
+
+    public boolean sacar(double valor) {
         if(!contaEstaAtiva()) {
-            return;
+            return false;
         }
-        if(!podeSacar(valor)) {
-            System.out.println(getTitular() +" Nao foi possivel sacar quantia.");
-            return;
+        if(!removerSaldo(valor)) {
+            return false;
         }
-        System.out.println(getTitular() +" sacou R$" +valor);
-        removerSaldo(valor);
-        historico.add( "-" + String.valueOf(valor));
+        historico.add("Saque: -" + valor);
+        return true;
     }
-    protected void transferir(double valor, ContaBase destino) {
+    public boolean depositar(double valor) {
         if(!contaEstaAtiva()) {
-            return;
+            return false;
         }
-        if(this == destino) {
-            System.out.println(getTitular() +" Nao e possivel tranferir para si mesmo. Tente fazer um deposito");
-            return;
+        if(!adicionarSaldo(valor)) {
+            return false;
         }
-        if(podeSacar(valor)) {
-            System.out.println(getTitular() +" tranferiu R$" +valor +" para " +destino.getTitular());
-            removerSaldo(valor);
-            historico.add( "-" + String.valueOf(valor));
-            destino.adicionarSaldo(valor);
-            destino.historico.add( "+" + String.valueOf(valor));
-        }
+        historico.add("Depósito: +" + valor);
+        return true;
     }
-    protected void mostrarConta() {
-        System.out.println("Titular: " +getTitular() +"/ Saldo: R$" +getSaldo() +"/ Limite da conta: R$" +valorMaximo +"/ Senha: ********");
-    }
-    protected void mostrarHistorico() {
-        System.out.println("Historico da conta: " +getTitular());
-        for(int i = 0;i < historico.size(); i ++) {
-            System.out.println(historico.get(i));
+    public boolean transferir(ContaBase destino, double valor) {
+        if(!contaEstaAtiva()) {
+            return false;
         }
+        if(!removerSaldo(valor)) {
+            System.out.println("Nao foi possivel transferir");
+            return false;
+        }
+        if(!destino.adicionarSaldo(valor)) {
+            adicionarSaldo(valor);
+            System.out.println("Nao foi possivel transferir");
+            return false;
+        }
+        historico.add("Transferencia enviada: -" + valor);
+        destino.historico.add("Transferencia recebida: +" + valor);
+        System.out.println(getNome() + " transferiu R$ " + valor + " para " + destino.getNome());
+        return true;
     }
+
 
 }
