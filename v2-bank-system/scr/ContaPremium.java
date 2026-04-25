86b7d986deeda35a4fc744a866a7fad4300ac3b7
Koehler <bernardoskoehler@gmail.com>
2026-04-25 12:37:05 -0300

    Create ContaPremium.java

---
+++ v2-bank-system/scr/ContaPremium.java
@@ -0,0 +1,12 @@
+public class ContaPremium extends ContaBase {
+
+    ContaPremium(String nome, String senha) {
+        super(nome, senha);
+        this.limite = 7500;
+    }
+
+    @Override
+    protected double valorTaxado(double valor) {
+        return valor * 0.97;
+    }
+}
