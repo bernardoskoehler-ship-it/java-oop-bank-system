41d7c8669fd69761fa81b4f6b588f10498f9eb83
Koehler <bernardoskoehler@gmail.com>
2026-04-25 12:36:21 -0300

    Update ContaCorrente.java

--- v2-bank-system/scr/ContaCorrente.java
+++ v2-bank-system/scr/ContaCorrente.java
@@ -1,1 +1,23 @@
+public class ContaCorrente extends ContaBase{
 
+    ContaCorrente(String nome, String senha) {
+        super(nome, senha);
+        this.limite = 2500;
+    }
+
+    @Override
+    public boolean podeRemoverSaldo(double valor) {
+        if(!valorValido(valor)) {
+            return false;
+        }
+        if(valor > limite) {
+            System.out.println("R$ " +valor +" esta acima do limite da conta");
+            return false;
+        }
+        if((getSaldo() - valor) < -500) {
+            System.out.println("Nao tem saldo o suficiente!");
+            return false;
+        }
+        return true;
+    }
+}
