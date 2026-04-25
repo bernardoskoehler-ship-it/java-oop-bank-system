5600d2e5eac63d0c7b213e095b305a89257e970c
Koehler <bernardoskoehler@gmail.com>
2026-04-25 14:09:03 -0300

    Update Banco.java

--- v2-bank-system/scr/Banco.java
+++ v2-bank-system/scr/Banco.java
@@ -3,15 +3,63 @@ import java.util.Map;
 
 
 public class Banco {
-    HashMap<String, ContaBase> contas = new HashMap<>();
+    private Map<String, ContaBase> contas = new HashMap<>();
 
-    public void adicionarConta(int tipo) {
-        if(tipo <= 0 || tipo > 3) {
-            System.out.println("Tipo de conta invalida");
+
+    public boolean contaExistente(String nome) {
+        if(nome == null || nome.isBlank()) {
+            System.out.println("Nome invalido!");
+            return false;
+        }
+        return contas.containsKey(nome);
+    }
+
+    public ContaBase buscarConta(String nome) {
+        return contas.get(nome);
+    }
+    public void mostrarConta(String nome) {
+        ContaBase conta = buscarConta(nome);
+
+        if (conta == null) {
+            System.out.println("Nao existe uma conta com esse ID!");
             return;
         }
 
+        System.out.println("Nome: " + conta.getNome());
+        System.out.println("Saldo: R$ " + conta.getSaldo());
+        System.out.println("Conta ativa: " + conta.contaEstaAtiva());
     }
 
+    public boolean criarConta(String nome, String senha, String tipo) {
+        if(tipo == null || tipo.isBlank()) {
+            System.out.println("Tipo de conta invalido!");
+            return false;
+        }
+        if(contaExistente(nome)) {
+            System.out.println("Ja existe uma conta com esse ID!");
+            return false;
+        }
 
+        ContaBase novaConta;
+        switch (tipo.toLowerCase()) {
+            case "corrente":
+                novaConta = new ContaCorrente(nome, senha);
+                break;
+
+            case "poupanca":
+                novaConta = new ContaPoupanca(nome, senha);
+                break;
+
+            case "premium":
+                novaConta = new ContaPremium(nome, senha);
+                break;
+
+            default:
+                System.out.println("Tipo de conta invalido!");
+                return false;
+        }
+        contas.put(nome, novaConta);
+        System.out.println("Conta criada com sucesso!");
+        return true;
+    }
 }
