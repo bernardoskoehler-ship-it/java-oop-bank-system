ca1cf5cd4d33d88280c10ede35d31fed071aaa16
Koehler <bernardoskoehler@gmail.com>
2026-04-25 12:36:34 -0300

    Update ContaPoupanca.java

--- v2-bank-system/scr/ContaPoupanca.java
+++ v2-bank-system/scr/ContaPoupanca.java
@@ -1,1 +1,19 @@
+public class ContaPoupanca extends ContaBase{
 
+    private double taxaRendimento = 0.005;
+
+    ContaPoupanca(String nome, String senha) {
+        super(nome, senha);
+        this.limite = 1000;
+    }
+    public boolean aplicarRendimento() {
+        double rendimento = getSaldo() * taxaRendimento;
+        if(rendimento <= 0) {
+            System.out.println("Nao foi possivel aplicar rendimento.");
+            return false;
+        }
+        depositar(rendimento);
+        System.out.println("Rendimento aplicado: R$ " + rendimento);
+        return true;
+    }
+}
