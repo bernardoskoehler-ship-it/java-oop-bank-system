131a25fe8baa046065e7477b8afb002c76607509
Koehler <bernardoskoehler@gmail.com>
2026-04-25 12:37:21 -0300

    Update Banco.java

--- v2-bank-system/scr/Banco.java
+++ v2-bank-system/scr/Banco.java
@@ -5,6 +5,13 @@ import java.util.Map;
 public class Banco {
     HashMap<String, ContaBase> contas = new HashMap<>();
 
+    public void adicionarConta(int tipo) {
+        if(tipo <= 0 || tipo > 3) {
+            System.out.println("Tipo de conta invalida");
+            return;
+        }
+
+    }
 
 
 }
