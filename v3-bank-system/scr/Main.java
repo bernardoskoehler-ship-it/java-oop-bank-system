556c5dddc650f935a1fbfe1c9f4b63e53402a7c3
Koehler <bernardoskoehler@gmail.com>
2026-06-10 12:06:35 -0300

    Update Main.java

--- v3-bank-system/scr/Main.java
+++ v3-bank-system/scr/Main.java
@@ -1,1 +1,17 @@
+package org.example;
 
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            ContaDAO dao = new ContaDAO();
+            dao.criar_TabelaConta();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        MenuBanco mb = new MenuBanco();
+        mb.iniciarBanco();
+
+    }
+}
